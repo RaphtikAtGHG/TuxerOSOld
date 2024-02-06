46ebc44ad125a1f36636bb9b3049504e387146aa
ConCrafter20 <raphael.wegmann81@gmail.com>
2024-02-06 21:30:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tuxer OS
+A simple 32-Bit BIOS based TUI Operating System. Using FAT-32!
---
+++ src/kernel/main.c
@@ -0,0 +1,49 @@
+#include <stdint.h>
+#include "stdio.h"
+#include "memory.h"
+#include <hal/hal.h>
+#include <arch/i686/irq.h>
+#include <debug.h>
+#include <boot/bootparams.h>
+
+extern void _init();
+
+void crash_me();
+
+void timer(Registers* regs)
+{
+    printf(".");
+}
+
+void start(BootParams* bootParams)
+{   
+    // call global constructors
+    _init();
+
+    HAL_Initialize();
+
+    log_debug("Main", "Boot device: %x", bootParams->BootDevice);
+    log_debug("Main", "Memory region count: %d", bootParams->Memory.RegionCount);
+    for (int i = 0; i < bootParams->Memory.RegionCount; i++) 
+    {
+        log_debug("Main", "MEM: start=0x%llx length=0x%llx type=%x", 
+            bootParams->Memory.Regions[i].Begin,
+            bootParams->Memory.Regions[i].Length,
+            bootParams->Memory.Regions[i].Type);
+    }
+
+
+    log_info("Main", "This is an info msg!");
+    log_warn("Main", "This is a warning msg!");
+    log_err("Main", "This is an error msg!");
+    log_crit("Main", "This is a critical msg!");
+    printf("Nanobyte OS v0.1\n");
+    printf("This operating system is under construction.\n");
+    //i686_IRQ_RegisterHandler(0, timer);
+    printf("Sup\n");
+
+    //crash_me();
+
+end:
+    for (;;);
+}
